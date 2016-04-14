d1ac9b00833fa8fa7f44a5cf1e61783a23c74b88
Rik Heywood <rik.heywood@pod-point.com>
2016-04-14 13:09:43 +0100

    Massive cleanup

--- js/jquery.circliful.js
+++ js/jquery.circliful.js
@@ -1,170 +1,144 @@
 "use strict";
 
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ 
+// requestAnimationFrame polyfill by Erik Möller
+// fixes from Paul Irish and Tino Zijdel
+ 
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 (function ($) {
 
     $.fn.circliful = function (options, callback) {
 
         var settings = $.extend({
             // These are the defaults.
-            //startDegree: 0,
-            foregroundColor: "#3498DB",
-            backgroundColor: "#ccc",
-            pointColor: "none",
-            fillColor: 'none',
-            foregroundBorderWidth: 15,
-            backgroundBorderWidth: 15,
-            pointSize: 28.5,
-            fontColor: '#aaa',
-            percent: 75,
-            animation: 1,
-            animationStep: 5,
-            icon: 'none',
-            iconSize: '30',
-            iconColor: '#ccc',
-            iconPosition: 'top',
-            target: 0,
-            start: 0,
-            showPercent: 1,
-            percentageTextSize: 22,
-            textAdditionalCss: '',
-            targetPercent: 0,
-            targetTextSize: 17,
-            targetColor: '#2980B9',
-            text: null,
-            textStyle: null,
-            textColor: '#666',
-            multiPercentage: 0,
-            percentages: null
+            foregroundColor: "#3498DB",     // Colour of the line
+            backgroundColor: "#ccc",        // Colour of the channel for the line to fill
+            fillColor: 'none',              // Fill the circle with this
+            pointColor: "none",             // Colour of the inner circle (around the percentage)
+            pointSize: 57,                  // Size of the inner circle
+            
+            foregroundBorderWidth: 15,      // Width of the line
+            backgroundBorderWidth: 15,      // Width of the lines channel
+            
+            percent: 75,                    // What percent if this chart going to represent
+
+            fontColor: '#aaa',              // Colour of the percentage text       
+            
+            animation: true,                // animate it?
+            animationStep: 5,               // Number of degrees to animate per step
+            
+            icon: 'none',                   // Show an icon in the middle or not
+            iconSize: 40,                   // Font size of the icon
+            iconColor: '#ccc',              // Colour of the icon
+            
+            showPercent: true,              // Show the percentage?
+            percentageTextSize: 22,         // Font size of the percentage
+            
+            text: null,                     // Additional text
+            textStyle: '',                  // Css for the additional text  
+            textColor: '#666'               // colour of the additional text
         }, options);
 
         return this.each(function () {
-            var circleContainer = $(this);
-            var percent = settings.percent;
-            var iconY = 83;
-            var iconX = 100;
-            var textY = 110;
-            var textX = 100;
-            var additionalCss;
-            var elements;
-            var icon;
-            var backgroundBorderWidth = settings.backgroundBorderWidth;
-
-            if(settings.iconPosition == 'bottom') {
-                iconY = 124;
-                textY = 95;
-            } else if(settings.iconPosition == 'left') {
-                iconX = 80;
-                iconY = 110;
-                textX = 117;
-            } else if(settings.iconPosition == 'middle') {
-                if(settings.multiPercentage == 1) {console.log(typeof settings.percentages == "object")
-                    if(typeof settings.percentages == "object") {
-                        backgroundBorderWidth = 30;
-                    } else {
-                        iconY = 110;
-                        elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="133" y1="50" x2="140" y2="40" stroke-width="2"  /></g>';
-                        elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="140" y1="40" x2="200" y2="40" stroke-width="2"  /></g>';
-                        textX = 228;
-                        textY = 47;
-                    }
-                } else {
-                    iconY = 110;
-                    elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="133" y1="50" x2="140" y2="40" stroke-width="2"  /></g>';
-                    elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="140" y1="40" x2="200" y2="40" stroke-width="2"  /></g>';
-                    textX = 175;
-                    textY = 35;
-                }
-            } else if(settings.iconPosition == 'right') {
-                iconX = 120;
-                iconY = 110;
-                textX = 80;
+            var circleContainer = $(this),
+                percent = settings.percent,
+                textY = 100,
+                textX = 100,
+                elements = '',
+                innerText = '',
+                radius = 80;
+            
+            // If we are showing an icon, move the % outside the circle
+            if (settings.icon != 'none' && settings.showPercent) {
+                elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="133" y1="50" x2="140" y2="40" stroke-width="2"  /></g>';
+                elements += '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="140" y1="40" x2="200" y2="40" stroke-width="2"  /></g>';
+                textX = 175;
+                textY = 25;
+                radius = 60;
             }
 
-            if(settings.targetPercent > 0) {
-                textY = 95;
-                elements = '<g stroke="' + (settings.backgroundColor != 'none' ? settings.backgroundColor : '#ccc') + '" ><line x1="75" y1="101" x2="125" y2="101" stroke-width="1"  /></g>';
-                elements += '<text text-anchor="middle" x="' + textX + '" y="120" style="font-size: ' + settings.targetTextSize + 'px;" fill="' + settings.targetColor + '">' + settings.targetPercent + '%</text>';
-                elements += '<circle cx="100" cy="100" r="69" fill="none" stroke="' + settings.backgroundColor + '" stroke-width="3" stroke-dasharray="450" transform="rotate(-90,100,100)" />';
-                elements += '<circle cx="100" cy="100" r="69" fill="none" stroke="' + settings.targetColor + '" stroke-width="3" stroke-dasharray="' + (360 / 100 * settings.targetPercent) + ', 20000" transform="rotate(-90,100,100)" />';
-
+            // Add the additional text
+            if (settings.text != null) {
+                elements += '<text text-anchor="middle" x="100" y="135" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
             }
 
-            if(settings.text != null && settings.multiPercentage == 0) {
-                elements += '<text text-anchor="middle" x="100" y="125" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
-            } else if(settings.text != null && settings.multiPercentage == 1) {
-                elements += '<text text-anchor="middle" x="228" y="65" style="' + settings.textStyle + '" fill="' + settings.textColor + '">' + settings.text + '</text>';
-            }
-
+            // Add the icon
             if (settings.icon != 'none') {
-                icon = '<text text-anchor="middle" x="' + iconX + '" y="' + iconY + '" class="icon" style="font-size: ' + settings.iconSize + 'px" fill="' + settings.iconColor + '">&#x' + settings.icon + '</text>';
+                innerText += '<text text-anchor="middle" dy="0.4em" x="100" y="100" class="icon" style="font-size: ' + settings.iconSize + 'px" fill="' + settings.iconColor + '">&#x' + settings.icon + '</text>';
             }
 
+            // Add the Percentage
+            if (settings.showPercent) {
+                innerText += '<text text-anchor="middle" dy="0.4em" x="' + textX + '" y="' + textY + '" class="timer" style="font-size: ' + settings.percentageTextSize + 'px;" fill="' + settings.fontColor + '">0%</text>';
+            }
+
+            // Length of the line (so we can animate it via dash pattern)
+            var strokeLength = 2 * radius * 3.1415927;
             circleContainer
                 .addClass('svg-container')
                 .append(
-                    $('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 194 186" class="circliful">' +
+                    $('<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 200 200" class="circliful">' +
                         elements +
-                        '<circle cx="100" cy="100" r="57" class="border" fill="' + settings.fillColor + '" stroke="' + settings.backgroundColor + '" stroke-width="' + backgroundBorderWidth + '" stroke-dasharray="360" transform="rotate(-90,100,100)" />' +
-                        '<circle class="circle" cx="100" cy="100" r="57" class="border" fill="none" stroke="' + settings.foregroundColor + '" stroke-width="' + settings.foregroundBorderWidth + '" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />' +
+                        '<circle cx="100" cy="100" r="'+radius+'" class="border" fill="' + settings.fillColor + '" stroke="' + settings.backgroundColor + '" stroke-width="' + settings.backgroundBorderWidth + '" stroke-dasharray="'+strokeLength+'" transform="rotate(-90,100,100)" />' +
+                        '<circle class="circle" cx="100" cy="100" r="'+radius+'" class="border" fill="none" stroke="' + settings.foregroundColor + '" stroke-width="' + settings.foregroundBorderWidth + '" stroke-dasharray="0,20000" transform="rotate(-90,100,100)" />' +
                         '<circle cx="100" cy="100" r="' + settings.pointSize + '" fill="' + settings.pointColor + '" />' +
-                        icon +
-                        '<text class="timer" text-anchor="middle" x="' + textX + '" y="' + textY + '" style="font-size: ' + settings.percentageTextSize + 'px; ' + additionalCss + ';' + settings.textAdditionalCss + '" fill="' + settings.fontColor + '">0%</text>')
+                        innerText)
                 );
 
-            var circle = circleContainer.find('.circle');
-            var myTimer = circleContainer.find('.timer');
-            var interval = 30;
-            var angle = 0;
-            var angleIncrement = settings.animationStep;
-            var last = 0;
-            var summary = 0;
-            var oneStep = 0;
+            var circle = circleContainer.find('.circle'),
+                myTimer = circleContainer.find('.timer'),
+                angle = 0;
 
-            if (settings.start > 0 && settings.target > 0) {
-                percent = settings.start / (settings.target / 100);
-                oneStep = settings.target / 100;
-            }
+            if (settings.animation) {
+                function drawFrame() {
+                    requestAnimationFrame(drawFrame);
 
-            if (settings.animation == 1) {
-                var timer = window.setInterval(function () {
-                    if ((angle) >= (360 / 100 * percent)) {
-                        window.clearInterval(timer);
-                        last = 1;
-                    } else {
-                        angle += angleIncrement;
-                        summary += oneStep;
+                    angle += settings.animationStep;
+                    if (angle >= 360 * percent / 100) {
+                        cancelAnimationFrame(drawFrame);
+                        angle = 360 * percent / 100;
                     }
 
-                    if (angle / 3.6 >= percent && last == 1) {
-                        angle = 3.6 * percent;
+                    circle.attr("stroke-dasharray", angle / 360 * strokeLength + ", 20000");
+
+                    if (settings.showPercent) {
+                        myTimer.text(parseInt(angle / 360 * 100) + '%');
                     }
+                }
 
-                    if (summary > settings.target && last == 1) {
-                        summary = settings.target;
-                    }
-
-                    circle
-                        .attr("stroke-dasharray", angle + ", 20000");
-
-                    if (settings.showPercent == 1) {
-                        myTimer
-                            .text(parseInt(angle / 360 * 100) + '%');
-                    } else {
-                        myTimer
-                            .text(summary);
-                    }
-
-                }.bind(circle), interval);
+                // trigger the animation
+                drawFrame();
             } else {
-                circle
-                    .attr("stroke-dasharray", (360 / 100 * percent) + ", 20000");
+                circle.attr("stroke-dasharray", (strokeLength * percent / 100) + ", 20000");
 
-                if (settings.showPercent == 1) {
-                    myTimer
-                        .text(percent + '%');
-                } else {
-                    myTimer
-                        .text(settings.target);
+                if (settings.showPercent) {
+                    myTimer.text(percent + '%');
                 }
             }
         });
